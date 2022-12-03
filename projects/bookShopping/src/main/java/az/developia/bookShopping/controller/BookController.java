54c9d7c11015576ab6415e7b467c433c3d943428
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 15:57:29 -0800

    kitablari cedvelde gostermek

---
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/BookController.java
@@ -0,0 +1,25 @@
+package az.developia.bookShopping.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import az.developia.bookShopping.dao.BookDAO;
+import az.developia.bookShopping.model.Book;
+
+@Controller
+public class BookController {
+@Autowired
+private BookDAO bookDAO;
+@GetMapping(path="/books")
+public String showBooks(Model model) {
+	List<Book> books=bookDAO.findAll();
+	model.addAttribute("books",books);
+	return "books";
+}
+
+
+}
