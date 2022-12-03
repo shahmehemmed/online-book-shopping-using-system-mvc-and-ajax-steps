3a282ea6770770e7db3d41f9d71550d08377a21e
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 13:40:56 -0800

    spring project created

---
+++ projects/bookShopping/src/test/java/az/developia/bookShopping/BookShoppingProjectForDevelopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShoppingProjectForDevelopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
