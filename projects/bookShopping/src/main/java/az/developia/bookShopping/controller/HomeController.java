b5f837326673d1c21f18427ed3e432a72d75049d
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 14:27:59 -0800

    home controller created
    Spring mvc view  yaratmaq addimlari

---
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/HomeController.java
@@ -0,0 +1,12 @@
+package az.developia.bookShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+@GetMapping(path="/home")
+public String showHomePage() {
+	return "home";
+}
+}
