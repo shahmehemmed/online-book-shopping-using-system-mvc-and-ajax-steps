014d5916dae90290eb41fa0f7d3467d527d794dc
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 17:29:31 -0800

    Yeni kitab  qeydiyyati uqurla edildi

--- projects/bookShopping/src/main/java/az/developia/bookShopping/controller/BookController.java
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/BookController.java
@@ -6,23 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookShopping.dao.BookDAO;
 import az.developia.bookShopping.model.Book;
 
 @Controller
 public class BookController {
-@Autowired
-private BookDAO bookDAO;
-@GetMapping(path="/books")
-public String showBooks(Model model) {
-	List<Book> books=bookDAO.findAll();
-	model.addAttribute("books",books);
-	return "books";
-}
-@GetMapping(path="/books/new")
-public String openNewBookPage() {
-	
-	return "new-book";
-}
+	@Autowired
+	private BookDAO bookDAO;
+
+	@GetMapping(path = "/books")
+	public String showBooks(Model model) {
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
+		return "books";
+	}
+
+	@GetMapping(path = "/books/new")
+	public String openNewBookPage(Model model) {
+		Book book = new Book();
+
+		model.addAttribute("book", book);
+
+		return "new-book";
+	}
+
+	@PostMapping(path = "/books/new-book-process")
+	public String saveBook(@ModelAttribute(name = "book") Book book,Model model) {
+		bookDAO.save(book);
+		List<Book> books = bookDAO.findAll();
+
+		model.addAttribute("books", books);
+
+		return "books";
+	}
 }
