bb60211dcf713c00dafbae1d94f04b19fa7a03c3
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-04 12:13:13 -0800

    diger sehifelerin yaradilmasi

---
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package az.developia.bookShopping.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import az.developia.bookShopping.dao.BookDAO;
+
+import az.developia.bookShopping.model.Book;
+
+@Controller
+public class CustomerController {
+
+	
+
+	@GetMapping(path = "/customer")
+	public String showCustomerPage() {
+		
+		return "customer";
+	}
+
+	
+}
