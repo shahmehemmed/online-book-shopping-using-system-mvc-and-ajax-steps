66a7903535fa83276ffd563ddfac0919adbe11d0
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 18:42:21 -0800

    Sehifelere kecid linki elave etmek

--- projects/bookShopping/src/main/java/az/developia/bookShopping/controller/HomeController.java
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-@GetMapping(path="/home")
+@GetMapping(path={"/home","/"})
 public String showHomePage() {
 	return "home";
 }
+
 }
