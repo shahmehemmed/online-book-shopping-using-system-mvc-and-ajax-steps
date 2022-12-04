4bce201c9a77a30818541c1a7589f5b9643a24dc
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-03 17:45:42 -0800

    kitab ucun qiymet elave edildi

--- projects/bookShopping/src/main/java/az/developia/bookShopping/model/Book.java
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/model/Book.java
@@ -12,6 +12,15 @@ public class Book {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private String description;
+	private double price;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -24,6 +33,13 @@ public class Book {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
 	
 	
 }
