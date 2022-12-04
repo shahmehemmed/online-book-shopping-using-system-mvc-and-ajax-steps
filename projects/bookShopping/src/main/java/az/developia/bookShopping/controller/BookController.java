5161b02ec647297502169f46359830db6ca1eb91
Shahmehemmed Balayev <shahmehemmed93@gmail.com>
2022-12-04 05:24:33 -0800

    kitabin silinmesi birinci hisse

--- projects/bookShopping/src/main/java/az/developia/bookShopping/controller/BookController.java
+++ projects/bookShopping/src/main/java/az/developia/bookShopping/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookShopping.dao.BookDAO;
@@ -42,6 +43,11 @@ public class BookController {
 
 		model.addAttribute("books", books);
 
+		return "books";
+	}
+	@GetMapping(path = "/books/delete/{id}")
+	public String deleteBook(@PathVariable(name="id")Integer id,Model model) {
+				System.out.println(id);
 		return "books";
 	}
 }
